d6f439ec80cb080eb1771cd15026da22b7d236aa
Chenfan <1978411520@qq.com>
2022-12-06 16:04:43 +0800

    1. init gateway

---
+++ src/main/java/com/c/ccloudgateway/CCloudGatewayApplication.java
@@ -0,0 +1,16 @@
+package com.c.ccloudgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author C
+ */
+@SpringBootApplication
+public class CCloudGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CCloudGatewayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/c/ccloudgateway/CCloudGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.c.ccloudgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CCloudGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
